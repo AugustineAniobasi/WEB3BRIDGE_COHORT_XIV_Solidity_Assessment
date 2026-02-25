1e61e790e28148a55b8055feac189c90079f68f9
AugustineAniobasi <augustineaniobasi@gmail.com>
2026-02-25 13:12:26 +0000

    feat: added name(), symbol(), decimal(), totalSUpply()

--- solidty_class/day2/assignment1.sol
+++ solidty_class/day2/assignment1.sol
@@ -1,146 +1,59 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
-contract ERC20 {
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+/**
+ *@IERC20 contains functions my contract must implement
+ *
+ **/
 
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
+interface IERC20 {
 
-    uint256 private _totalSupply;
+function name() external view returns(string memory);
+function symbol() external view returns(string memory);
+function decimals() external view returns(uint8);
+function totalSupply() external view returns(uint256);
+function balanceOf() external view returns(uint256 balance);
+function transfer(address _to, uint256 _value) external returns(bool success);
+function transferFrom(address _from, address _to, uint256 _value) external returns(bool success);
+function approve(address _spender, uint256 _value) external returns(bool success);
+function allowance(address _owner, address _spender) external view returns(uint256 remaining);
 
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
+}
+contract PacoinToken is IERC20 {
 
-    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
-        _name = name_;
-        _symbol = symbol_;
-        _decimals = decimals_;
-    }
+string private _name;
+string private _decimals;
+string private _symbol;
+uint256 private _totalSUpply;
 
-    function name() public view returns (string memory) {
-        return _name;
-    }
+constructor() {
 
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    function decimals() public view returns (uint8) {
-        return _decimals;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-
-    function transfer(address to, uint256 amount) public returns (bool) {
-        _transfer(msg.sender, to, amount);
-        return true;
-    }
-
-    function allowance(address owner, address spender) public view returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 amount) public returns (bool) {
-        _approve(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
-        uint256 currentAllowance = _allowances[from][msg.sender];
-        require(currentAllowance >= amount, "ERC20: insufficient allowance");
-
-        // Decrease allowance (standard behavior)
-        unchecked {
-            _approve(from, msg.sender, currentAllowance - amount);
-        }
-
-        _transfer(from, to, amount);
-        return true;
-    }
-
-    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
-        _approve(msg.sender, spender, _allowances[msg.sender][spender] + addedValue);
-        return true;
-    }
-
-    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
-        uint256 currentAllowance = _allowances[msg.sender][spender];
-        require(currentAllowance >= subtractedValue, "ERC20: decreased below zero");
-        unchecked {
-            _approve(msg.sender, spender, currentAllowance - subtractedValue);
-        }
-        return true;
-    }
-
-    function _transfer(address from, address to, uint256 amount) internal {
-        require(from != address(0), "ERC20: transfer from zero");
-        require(to != address(0), "ERC20: transfer to zero");
-        require(amount > 0, "ERC20: amount zero");
-
-        uint256 fromBal = _balances[from];
-        require(fromBal >= amount, "ERC20: insufficient balance");
-
-        unchecked {
-            _balances[from] = fromBal - amount;
-        }
-        _balances[to] += amount;
-
-        emit Transfer(from, to, amount);
-    }
-
-    function _approve(address owner, address spender, uint256 amount) internal {
-        require(owner != address(0), "ERC20: approve from zero");
-        require(spender != address(0), "ERC20: approve to zero");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
-    function _mint(address to, uint256 amount) internal {
-        require(to != address(0), "ERC20: mint to zero");
-        require(amount > 0, "ERC20: amount zero");
-
-        _totalSupply += amount;
-        _balances[to] += amount;
-
-        emit Transfer(address(0), to, amount);
-    }
-
-    function _burn(address from, uint256 amount) internal {
-        require(from != address(0), "ERC20: burn from zero");
-        require(amount > 0, "ERC20: amount zero");
-
-        uint256 fromBal = _balances[from];
-        require(fromBal >= amount, "ERC20: burn exceeds balance");
-
-        unchecked {
-            _balances[from] = fromBal - amount;
-        }
-        _totalSupply -= amount;
-
-        emit Transfer(from, address(0), amount);
-    }
 }
 
-contract ExampleToken is ERC20 {
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        uint8 decimals_,
-        uint256 initialSupply_
-    ) ERC20(name_, symbol_, decimals_) {
-        // initialSupply_ is in the smallest unit (like wei for ETH)
-        _mint(msg.sender, initialSupply_);
-    }
+/**
+ *
+ *@name(), symbol(), decimals() are optional functions
+ *@totalSupply(),transfer(),transferFrom(),approve(),balanceOf() and allowance()
+ *Are all compulsory functions that must be implemented in every ERC20 token
+ *
+ **/
+
+function name() public view returns(string memory) {
+    return _name;
+} 
+
+function symbol() public view returns(string memory) {
+    return _symbol;
 }
 
+function decimals() public view returns(uint8) {
+    return _decimals;
+
+function totalSupply() public view returns(uint256) {
+    return _totalSupply;
+}
+
+}
+
+
+}
