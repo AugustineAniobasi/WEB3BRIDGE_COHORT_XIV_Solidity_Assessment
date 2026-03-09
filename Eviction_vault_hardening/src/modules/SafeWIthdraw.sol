7fcf53ed133be5f4b164773f3e023b9f569f91bd
AugustineAniobasi <augustineaniobasi@gmail.com>
2026-03-09 16:43:23 +0000

    feat: SafeWithdraw replace .transfer() to .call{value:amount}() because  .transfer can break due to gas changes

---
+++ Eviction_vault_hardening/src/modules/SafeWIthdraw.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../core/VaultStorage.sol";
+
+abstract contract SafeWithdraw is VaultStorage {
+
+    event Withdrawal(address indexed user, uint256 amount);
+
+    function withdraw(uint256 amount) external {
+
+        require(!paused, "paused");
+        require(balances[msg.sender] >= amount);
+
+        balances[msg.sender] -= amount;
+        totalVaultValue -= amount;
+
+        (bool success,) = msg.sender.call{value: amount}("");
+        require(success, "transfer failed");
+
+        emit Withdrawal(msg.sender, amount);
+    }
+}
